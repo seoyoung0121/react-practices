691d64fd1be8149032da2ca6fe81d4ebc67044ca
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-02-11 16:51:32 +0900

    03, 04, 05 - property, style, event

--- 05.Component - Event/ex04/src/App.js
+++ 05.Component - Event/ex04/src/App.js
@@ -1,13 +1,75 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import logo from './assets/images/react-logo.png';
 
 export default function App() {
+    const imgRef = useRef(null);
+    const onKeyUpInput = (e) => {
+        if (e.key = 'Enter') {
+            console.log('keyup:' + e.target.value);
+        }
+    };
+
+    const onKeyDownInput = (e) => {
+        console.log('keydown:' + e.target.value);
+    };
+
+    const onChangeInput = (e) => {
+        console.log('change:' + e.target.value);
+    };
+
+    const onFocusInput = (e) => {
+        console.log('focus');
+    };
+
+    const onBlurInput = (e) => {
+        console.log('blur');
+    };
+
+    const onMouseOverImg = (e) => {
+        const offsetTop = imgRef.current.offsetTop;
+        const offsetLeft = imgRef.current.offsetLeft;
+
+        console.log('mouseover', `x=${e.clientX - offsetLeft}, y=${e.clientY- offsetTop}`);
+    }
+
+    const onMouseMoveImg = (e) => {
+         const offsetTop = imgRef.current.offsetTop;
+        const offsetLeft = imgRef.current.offsetLeft;
+        console.log('mousemove', `x=${e.clientX - offsetLeft}, y=${e.clientY- offsetTop}`);
+    }
+
+    const onMouseOutImg = (e) => {
+        console.log('mouseout', `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onMouseDownImg = (e) => {
+       console.log('mousedown', `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onMouseUpImg = (e) => {
+       console.log('mouseup', `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onClickImg = (e) => {
+       console.log('click', `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onDoubleClickImg = (e) => {
+       console.log('doubleclick', `x=${e.clientX}, y=${e.clientY}`);
+    }
+
     return (
         <>
             <h2>Event Handler 예제</h2>
             <input
                 type='text'
-                placeholder='메세지를 입력 하세요'/>
+                placeholder='메세지를 입력 하세요'
+                onKeyDown={onKeyDownInput}
+                onKeyUp={onKeyUpInput}
+                onChange={onChangeInput}
+                onFocus={onFocusInput}
+                onBlur={onBlurInput}
+            />
             <br/>
             <br/>
             <img
@@ -17,7 +79,16 @@ export default function App() {
                     width: 190,
                     border: '1px solid #ccc'
                 }}
-                src={logo}/>
+                src={logo}
+                onMouseOver={onMouseOverImg}
+                onMouseMove={onMouseMoveImg}
+                onMouseOut={onMouseOutImg}
+                onMouseDown={onMouseDownImg}
+                onMouseUp={onMouseUpImg}
+                onClick={onClickImg}
+                onDoubleClick={onDoubleClickImg}
+            />
+            
         </>
     );
 }
