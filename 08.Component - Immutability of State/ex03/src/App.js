918cd3836f1a77e7b4df9fd3f73767e7c368160e
seoyoung0121 <veronica9468@naver.com>
2025-02-14 13:42:20 +0900

    08 - ex03

--- 08.Component - Immutability of State/ex03/src/App.js
+++ 08.Component - Immutability of State/ex03/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import data from './assets/json/data.js';
+import update from 'react-addons-update';
+
+function GoodList({goods}){
+    const goodsUpdated=goods.concat({
+        "no": "p002-001",
+        "name": "팬츠스트라이프",
+        "price": 2000,
+        "amount": 1
+      });
+    return(
+       <ul>
+         {goodsUpdated.map((g,i)=><li key={i}>{`${g.name}:${g.amount}:${g.price}`}</li>)}
+       </ul>
+    )
+}
 
 function App() {
-
+    const [order, setOrder]=useState(data);
     return (
         <div id='App'>
-            <p>{`배송지:`}</p>
-            <p>{`결제수단:`}</p>
+            <p>{`배송지: ${order.receive}`}</p>
+            <p>{`결제수단: ${order.payment.method}`}</p>
             <p>{'상품'}</p>
-            <button>
+            <GoodList goods={order.goods}/>
+            <button onClick={()=>{
+                setOrder(update(order,{
+                    receive:{
+                        $set:"dasfasdf"
+                    }
+                }))
+            }}>
                 {"배송지 수정"}
             </button>
         </div>
